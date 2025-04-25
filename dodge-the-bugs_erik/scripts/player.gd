5de3540f831d0f86af3e070d545b6ea710c1eb84
piRIKIT0 <134180830+piRIKIT0@users.noreply.github.com>
2025-04-25 08:32:58 -0300

    Add files via upload
    ATUALIZADO

--- dodge-the-bugs_erik/scripts/player.gd
+++ dodge-the-bugs_erik/scripts/player.gd
@@ -1,6 +1,10 @@
 extends Area2D
 
+signal hit
 const SPEED := 400
+@onready var screen_size = get_viewport_rect().size
+@onready var anim = $anim
+@onready var collision = $collision
 
 func _ready():
 	pass
@@ -10,5 +14,33 @@ func _process(delta):
 	
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * SPEED
-		
+	
+	if velocity.x != 0:
+		anim.play("move")
+	elif velocity.y > 0:
+		anim.play("move_up")
+	elif velocity.y < 0:
+		anim.play("move_down")
+	else:
+		anim.play("idle")
+	
+	#if velocity.x > 0:
+		#anim.flip_h = false
+	#else:
+		#anim.flip_h = true
+	
+	anim.flip_h = true if velocity.x > 0 else false 
+	
 	position += velocity * delta
+	position = position.clamp(Vector2.ZERO, screen_size)
+
+#Verificação da colisão do player com os Bugs
+func _on_body_entered(body: Node2D) -> void:
+	hide()
+	hit.emit()
+	collision.set_deferred("disabled", true)
+
+func start_pos(pos):
+	position = pos
+	show()
+	collision.disabled = false
